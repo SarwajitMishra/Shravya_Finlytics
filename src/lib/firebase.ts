2dba36370b5aad780ae10e784d87c08a63d458c8
Sarwajit Mishra <sarwajit.mishra.cvrce@gmail.com>
2025-07-16 06:08:33 +0000

    It is done, except measurementId everything else is filled

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID",
+  apiKey: "AIzaSyDECQuiac9obpasO2BLpf5hIfneqSxvpXw",
+  authDomain: "shravya-finlytics.firebaseapp.com",
+  projectId: "shravya-finlytics",
+  storageBucket: "shravya-finlytics.firebasestorage.app",
+  messagingSenderId: "454569345015",
+  appId: "1:454569345015:web:08ffd65cabe49b1ea0b8f2",
   measurementId: "YOUR_MEASUREMENT_ID"
 };
 
