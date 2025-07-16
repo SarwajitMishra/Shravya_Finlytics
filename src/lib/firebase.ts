c3b4af56d99f8b671dad5276d8eb44315621c438
Sarwajit Mishra <sarwajit.mishra.cvrce@gmail.com>
2025-07-16 06:27:15 +0000

    Its not only sign up, Looks lie some access issue.

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -6,7 +6,7 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDECQuiac9obpasO2BLpf5hIfneqSxvpXw",
+  apiKey: "YOUR_NEW_API_KEY",
   authDomain: "shravya-finlytics.firebaseapp.com",
   projectId: "shravya-finlytics",
   storageBucket: "shravya-finlytics.firebasestorage.app",
