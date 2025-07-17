3bc31d6c4f224683310ecf1b42dfd27c2ddff27d
Sarwajit Mishra <sarwajit.mishra.cvrce@gmail.com>
2025-07-17 05:06:24 +0000

    Login is getting failed for incorrect api key

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -6,7 +6,7 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "YOUR_NEW_API_KEY",
+  apiKey: "YOUR_API_KEY_HERE",
   authDomain: "shravya-finlytics.firebaseapp.com",
   projectId: "shravya-finlytics",
   storageBucket: "shravya-finlytics.firebasestorage.app",
